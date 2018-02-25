07e04d2507f4f176f7b8a9201efd796a45ddaf67
Omar First App <omar.amrani2@mail.dcu.ie>
2018-02-25 23:18:54 +0000

    finishing create action and creating show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,17 +10,33 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
 
-    redirect_to article_path(@article)
+      flash[:notice] = "Article was successfully created"
+
+      redirect_to article_path(@article)
+
+    else
+
+      render 'new'
+
+    end
 
   end
+
+
 
   private
 
   def article_params
 
     params.require(:article).permit(:title, :description)
+
+  end
+
+  def show
+
+    @article = Article.find(params[:id])
 
   end
 
