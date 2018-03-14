324cef0acc0e87d3950995c3a566aa56909db36d
Omar First App <omar.amrani2@mail.dcu.ie>
2018-03-14 23:25:09 +0000

    Restriction from the UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] #return current user if doesn't exist hit the db
+  end
+  def logged_in?
+    !!current_user #convert to boolean
+  end
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new
   end
@@ -13,10 +15,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -26,12 +28,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+
     @user_articles= @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   private
