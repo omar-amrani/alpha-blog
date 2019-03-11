73ac1681df9ded47d76ee0232146e7b8d838ca0f
omar-amrani <omar.amrani@intercom.io>
2019-03-11 11:58:26 +0000

    test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 
   def index
 
-    @articles = Article.paginate(page: params[:page], per_page: 18)
+    @articles = Article.paginate(page: params[:page], per_page: 5)
     #test youo
     # another test woop woop
     # another test 6787
