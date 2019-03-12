da54ef25ba6aaff9b58fb357eb4bbf5a56689a56
omar-amrani <omar.amrani@intercom.io>
2019-03-12 20:24:03 +0000

    Revert "another test"
    This reverts commit 90ac28558878a7b0182405ce4db2a85ca235ff56.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,8 @@ class ArticlesController < ApplicationController
   def index
 
     @articles = Article.paginate(page: params[:page], per_page: 5)
-    #test
+    #test youo
+    # another test woop woop
   end
 
 
